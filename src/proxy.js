e0eb3179bf1f555c21ee18170e69f8c71d55009e
mohammadmostofa <mohammadmostofa.dev@gmail.com>
2026-05-22 22:02:05 +0600

    proxy issue try solve

--- src/proxy.js
+++ src/proxy.js
@@ -2,8 +2,7 @@ import { NextResponse } from 'next/server'
 import { auth } from './lib/auth';
 import { headers } from 'next/headers';
  
-// This function can be marked `async` if using `await` inside
-export async function proxy(request) {
+export async function middleware(request) {
  const session = await auth.api.getSession({
          headers: await headers()
      });
