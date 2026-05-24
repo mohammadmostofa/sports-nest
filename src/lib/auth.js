3bf4f5d24a205e792cbeb4b66125ce2e8d1a5e0f
mohammadmostofa <mohammadmostofa.dev@gmail.com>
2026-05-24 17:53:38 +0600

    added in nothing

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,7 +1,7 @@
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
-// import { jwt } from "better-auth/plugins";
+
 
 const client = new MongoClient(process.env.MONGODB_URI);
 const db = client.db('sports-nest');
@@ -14,7 +14,6 @@ export const auth = betterAuth({
     emailAndPassword: { 
     enabled: true, 
   }, 
-
       socialProviders: {
         google: { 
             clientId: process.env.GOOGLE_CLIENT_ID, 
